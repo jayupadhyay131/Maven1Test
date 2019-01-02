a33addc55c94427e3bc68443f45995f248fd692e
Jay Upadhyay <jay@getventive.com>
2019-01-01 18:23:33 -0700

    First time pushing code for maven test project

---
+++ src/test/java/com/qa/test/DemoTest2.java
@@ -0,0 +1,17 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class DemoTest2 {
+	
+	@Test
+	public void sum() {
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(30, a+b);
+		System.out.println("TEST CASE 2 --------------");
+	}
+	
+}
